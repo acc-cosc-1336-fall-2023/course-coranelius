2fd1142d1cc944519b1a242963c19bdbd7bfbe8f
chrisblalock <113274242+coranelius@users.noreply.github.com>
2023-09-12 00:07:38 +0000

    hw1 variable expressions

--- run_tests.py
+++ run_tests.py
@@ -4,6 +4,7 @@ the file in /tests/homework/b_in_proc_out/tests_in_proc_out
 has the test functions
 '''
 from tests.homework.b_in_proc_out import tests_in_proc_out
+#from tests.examples.c_decisions import test_decisions
 
 suite = unittest.TestLoader().loadTestsFromModule(tests_in_proc_out)
 unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/examples/a_example/tests_devprocess.py
+++ tests/examples/a_example/tests_devprocess.py
@@ -1,9 +1,59 @@
 import unittest
+import devprocess
 
-from src.examples.a_example.devprocess import add_numbers
+from src.examples.a_example.devprocess import add_numbers, get_remainder, operator_precedence_1, square_value
+from src.examples.a_example.devprocess import echo_number
+from src.examples.a_example.devprocess import floating_point_division
 
 class Test_Config(unittest.TestCase):
 
     def test_add_numbers_2(self):
         self.assertEqual(2, add_numbers(1, 1))
+
+    def test_add_numbers_100(self):
+        self.assertEqual(100, add_numbers(50, 50))
+
+    def test_add_numbers_200(self):
+        self.assertEqual(200, add_numbers(50, 150))
+
+    def test_echo_number_5(self):
+        self.assertEqual(5, echo_number(5))
+
+    def test_echo_number_500(self):
+        self.assertEqual(500, echo_number(500))
+
+    def test_echo_number_5000(self):
+        self.assertEqual(5000, echo_number(5000))
+    
+    def test_echo_number_5000(self):
+        self.assertEqual(5000, echo_number(5000))
+
+    def test_floating_point_division_1(self):
+        self.assertEqual(2.5, floating_point_division(5, 2))
+
+    def test_floating_point_division_2(self):
+        self.assertEqual(2.5, floating_point_division(20, 8))
+
+    def test_integer_division_1(self):
+        self.assertEqual(2.5, floating_point_division(5, 2))
+    
+    def test_integer_division_2(self):
+        self.assertEqual(2, floating_point_division(20, 8))
+
+    def test_operator_precedence_1(self):
+        self.assertEqual(14, operator_precedence_1(12, 6, 3))
+
+    def test_operator_precedence_2(self):
+        self.assertEqual(6, operator_precedence_1(12, 6, 3))
+
+    def test_square_value_1(self):
+        self.assertEqual(4, square_value(2))
+        self.assertEqual(16, square_value(4))
+
+    def test_get_remainder(self):
+        self.assertEqual(0, get_remainder(2, 2)
+                         
+
+    def test_add_numbers_425(self):
+        self.assertEqual(4.25, add_numbers(2, 2.25)
 
