6ef7e5056fb79e505086ed3ab7753c4d233ac2c2
chrisblalock <113274242+coranelius@users.noreply.github.com>
2023-10-01 01:40:41 +0000

    HW 4 Time clock

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,15 @@
-#
+from value_return import get_hour
+from value_return import get_minutes
+from value_return import get_seconds
+
+
+epoch_seconds = 3800
+
+seconds = get_seconds(epoch_seconds)
+minutes = get_minutes(epoch_seconds)
+hours = get_hour(epoch_seconds)
+
+
+time = f"{hours}:{minutes}:{seconds}"
+
+print(time)
--- src/homework/e_functions/value_return.py
+++ src/homework/e_functions/value_return.py
@@ -1,1 +1,20 @@
-#
+
+def get_hour(epoch_seconds):
+    hours = (epoch_seconds // 3600) % 24
+    if hours < 10:
+        hours = "0" + str(hours)
+    return hours
+    
+
+def get_minutes(epoch_seconds):
+    minutes = (epoch_seconds // 60) % 60
+    if minutes < 10:
+        minutes = "0" + str(minutes)
+    return minutes
+
+def get_seconds(epoch_seconds):
+    seconds = epoch_seconds % 60
+    
+    if seconds < 10:
+        seconds = "0" + str(seconds)
+    return seconds
