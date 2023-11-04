0e513bcf65deedf6ff664b71e82b328fa501ec97
chrisblalock <113274242+coranelius@users.noreply.github.com>
2023-11-04 17:27:55 +0000

    llists and tuples check through HW 8

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,25 @@
-#
+import unittest
+
+baseball = set(['Jodi', 'Carmen', 'Aida', 'Alicia'])
+basketball = set(['Eva', 'Carmen', 'Alicia', 'Sarah'])
+
+class TestSetOperations(unittest.TestCase):
+    def test_intersection(self):
+        result = baseball.intersection(basketball)
+        self.assertEqual(result, {'Carmen', 'Alicia'})
+
+    def test_union(self):
+        result = baseball.union(basketball)
+        self.assertEqual(result, {'Jodi', 'Carmen', 'Aida', 'Alicia', 'Eva', 'Sarah'})
+
+    def test_difference_baseball(self):
+        result = baseball.difference(basketball)
+        self.assertEqual(result, {'Jodi', 'Aida'})
+
+    def test_difference_basketball(self):
+        result = basketball.difference(baseball)
+        self.assertEqual(result, {'Eva', 'Sarah'})
+
+    def test_symmetric_difference(self):
+        result = baseball.symmetric_difference(basketball)
+        self.assertEqual(result, {'Jodi', 'Aida', 'Eva', 'Sarah'})
